edb3103d9339841ddf58014852460f29912908f7
Zak <zachary.strassberg@abbvie.com>
2020-05-16 00:56:52 -0400

    styles

--- src/style/index.ts
+++ src/style/index.ts
@@ -1,7 +1,13 @@
 import { css } from "emotion";
 
-export const unstyleList = css({});
-
+export const unstyleList = css({
+  margin: 0,
+  padding: 0,
+  listStyle: "none",
+});
+export const horizontalList = css({
+  display: "flex",
+});
 export const smallCaps = css({ fontVariant: "all-small-caps" });
 export const bold = css({ fontWeight: 700 });
 export const flexRow = css({ display: "flex", flexDirection: "row" });
